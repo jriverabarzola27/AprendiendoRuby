310264bc639df8fca786a16cdaa0fa308d58fc67
jriverabarzola27 <jriverabarzola@gmail.com>
2020-02-04 17:47:20 -0500

    primer commit

---
+++ RUBY/loops.rb
@@ -0,0 +1,17 @@
+
+
+#While
+
+#x = 1
+#while x < 5 do
+#	puts "el numero es #{x}"
+#	x += 1
+#end
+
+
+#for i in 1..10 do
+#	puts "hola el numero es #{i}"
+#end
+
+
+[1,2,3,4,5].each { |x| puts "hola #{x}"}
