553d88260ae1da336ac749f3a6c95d93a9b5c98e
jriverabarzola27 <jriverabarzola@gmail.com>
2020-02-04 18:02:46 -0500

    segundo commit

--- RUBY/clases.rb
+++ RUBY/clases.rb
@@ -2,3 +2,48 @@ CLASES DE RUBY
 --------------
 
 
+es un lenguaje dinamico, interpretado y orientado a objetos
+
+Ventajas
+
+Comunidad muy grande
+Muchas librerías
+Constantes actualizaciones
+Lenguaje maduro
+Desventajas
+
+Interpretado puede significar lento
+Alto uso de memoria
+No paralelismo
+Ha venido decreciendo en popularidad
+
+
+----------------- Primeros metodos para ruby --------------------
+
+x = 1
+
+x.class  = explica todas las clases encontradas
+
+x.odd? = pregunta si la variable es impar
+
+x.even? = pregunta si la variable es para
+
+---- exponentes
+
++ = suma
+- = resta
+/ = división
+% = raiz
+** = potenciacion
+
+----------la convencion en ruby ----
+
+propiedad cliente
+
+--------------------------------------------------------------------
+
+declarando un string
+
+saludo = 'hola' or "hola"
+
+ 
